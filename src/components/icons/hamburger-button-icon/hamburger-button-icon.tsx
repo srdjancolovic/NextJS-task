2d248308d35b50b3aa6d19c21cbf7a83c17a1169
Srdjan Colovic <colovicsrdjan@yahoo.com>
2025-04-11 08:21:31 +0200

    Hamburger icon change

---
+++ src/components/icons/hamburger-button-icon/hamburger-button-icon.tsx
@@ -0,0 +1,32 @@
+const HamburgerButtonIcon = () => {
+    return (
+        <svg
+            width="32"
+            height="18"
+            viewBox="0 0 32 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M32 2H0V0H32V2Z"
+                fill="#154B50"
+            />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M19 10L-1.74846e-07 10L0 8L19 8L19 10Z"
+                fill="#0B828A"
+            />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M32 18H0V16H32V18Z"
+                fill="#154B50"
+            />
+        </svg>
+    );
+};
+
+export default HamburgerButtonIcon;
